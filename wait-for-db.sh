aa8581b2bf951fdfc1466711f94754bd8654e4a1
marinus-g <marinusmail@protonmail.com>
2024-03-02 20:48:45 +0100

    using a .env var now for the db password

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -8,7 +8,7 @@ host="$1"
 shift
 cmd="$@"
 
-until mysql -h "$host" -u admin -padminpw -e 'SELECT 1'; do
+until mysql -h "$host" -u admin -p ${DATABASE_ADMIN_PASSWORD} -e 'SELECT 1'; do
   >&2 echo "MariaDB is unavailable - sleeping"
   sleep 1
 done
